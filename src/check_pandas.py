2ec1afc475d7d434defcca771a51d178710f63c0
Rob <robert.cook@bcu.ac.uk>
2020-08-03 14:02:35 +0100

    Convert main to separate scripts for python and pandas.

---
+++ src/check_pandas.py
@@ -0,0 +1,6 @@
+try:
+    import pandas as pd
+    print("Pandas loaded successfully")
+except ModuleNotFoundError:
+    print("Package: 'pandas' not available.  Check internet access and re-run install script.")
+
