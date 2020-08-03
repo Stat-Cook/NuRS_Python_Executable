1544443cbf7c35d4ae79b77cd2eac5b073ee1162
Rob <robert.cook@bcu.ac.uk>
2020-08-03 14:03:55 +0100

    change routine.txt to routine.py

---
+++ routine.py
@@ -0,0 +1,10 @@
+import os
+
+# Check where the current working directory is:
+print(os.getcwd())
+
+DIR = "" # Change this to the
+
+execfile(DIR + "src/check_python.py", {})
+pip install pandas
+execfile(DIR + "src/check_pandas.py", {})
