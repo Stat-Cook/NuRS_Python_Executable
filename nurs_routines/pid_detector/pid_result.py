96aa51577c813f9592aca89d420e4ef2ce8b51c5
Rob <robert.cook@bcu.ac.uk>
2020-12-16 13:37:49 +0000

    Add docstrings

--- nurs_routines/pid_detector/pid_result.py
+++ nurs_routines/pid_detector/pid_result.py
@@ -1,17 +1,35 @@
+"""
+Dictionary of PID results
+"""
+
 from collections import defaultdict
 
 
 class PIDResults(defaultdict):
-
+    """
+    Dictionary of PID results
+    """
     def __repr__(self):
         return "PIDResults({})".format(self.size_summary)
 
     @property
     def size_summary(self):
+        """
+        Report summary of how often each pattern was used.
+        Returns
+        -------
+        dict
+        """
         return {i: len(j) for i, j in self.items()}
 
     @property
     def context_factory(self):
+        """
+        Report line by line how each PID pattern was used.
+        Returns
+        -------
+        yield
+        """
         for pattern, data in self.items():
             if len(data) > 0:
                 yield f"Pattern: {pattern}".format(pattern=pattern)
@@ -21,10 +39,26 @@ class PIDResults(defaultdict):
                     )
 
     def context_to_screen(self):
+        """
+        Print results of PID match to screen.
+        Returns
+        -------
+        None
+        """
         for context in self.context_factory:
             print(context)
 
     def context_to_file(self, filename):
+        """
+        Export PID results to file
+        Parameters
+        ----------
+        filename: str
+            Target file for export
+        Returns
+        -------
+        None
+        """
         with open(filename, "w") as f:
             for context in self.context_factory:
                 f.write(context)
