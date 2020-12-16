f4c34f8ec4b5122d94ba1e647a89df782d0aeecd
Rob <robert.cook@bcu.ac.uk>
2020-12-16 12:11:53 +0000

    Add docstring and white space.

--- nurs_routines/pid_detector/pid_interface.py
+++ nurs_routines/pid_detector/pid_interface.py
@@ -1,7 +1,11 @@
+"""
+Default PID functions.
+"""
+
 import os
 import pandas as pd
-from .pid_report import PIDReporter
 
+from .pid_report import PIDReporter
 
 PATH = os.path.dirname(os.path.realpath(__file__))
 PATTERNS = pd.read_table(os.path.join(PATH, "names.txt"), header=None).values.flatten()
