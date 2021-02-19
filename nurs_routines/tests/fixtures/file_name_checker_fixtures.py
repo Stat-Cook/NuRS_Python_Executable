1167994addf31be1f1aa2472798e3c4c77e61318
Rob <robert.cook@bcu.ac.uk>
2021-02-19 18:38:06 +0000

    Add fixtures for file_name_checker.

--- nurs_routines/tests/fixtures/file_name_checker_fixtures.py
+++ nurs_routines/tests/fixtures/file_name_checker_fixtures.py
@@ -1,7 +1,8 @@
+"""Fixtures for test_file_name_checker"""
 import os
 import pytest
 
-from nurs_routines.utilities.file_name_checker import FileNameChecker, check_file_names
+from nurs_routines.utilities.file_name_checker import FileNameChecker
 
 test_data_path = os.path.join("nurs_routines", "tests", "test_data", "file_names")
 
@@ -10,9 +11,6 @@ test_data_path = os.path.join("nurs_routines", "tests", "test_data", "file_names
 def file_name_checker_monthly():
     """
     Name checker class for testing monthly functions.
-    Returns
-    -------
-    FileNameChecker
     """
 
     name_checker = FileNameChecker("Monthly_Test", "Effective date",
@@ -24,9 +22,6 @@ def file_name_checker_monthly():
 def file_name_checker_monthly_fails():
     """
     File Name Checker to test monthly files fail.
-    Returns
-    -------
-    FileNameChecker
     """
 
     name_checker = FileNameChecker("Monthly_Test", "Effective date",
@@ -38,9 +33,6 @@ def file_name_checker_monthly_fails():
 def file_name_checker_quarterly():
     """
     File Name Checker to test quarterly files.
-    Returns
-    -------
-    FileNameChecker
     """
     name_checker = FileNameChecker("Quarterly_Test", "Effective date",
                                    "Quarterly", "Quarterly_data", prepend_path=test_data_path)
@@ -51,12 +43,10 @@ def file_name_checker_quarterly():
 def file_name_checker_quarterly_fails():
     """
     File Name Checker to test quarterly files fail.
-    Returns
-    -------
-    FileNameChecker
     """
     name_checker = FileNameChecker("Quarterly_Test", "Effective date",
-                                   "Quarterly", "Quarterly_data_failure", prepend_path=test_data_path)
+                                   "Quarterly", "Quarterly_data_failure",
+                                   prepend_path=test_data_path)
     return name_checker
 
 
@@ -64,9 +54,6 @@ def file_name_checker_quarterly_fails():
 def file_name_checker_full():
     """
     File Name Checker to test full file names.
-    Returns
-    -------
-    FileNameChecker
     """
     name_checker = FileNameChecker("Full_Test", "Time Series",
                                    "Full", "", prepend_path=test_data_path)
