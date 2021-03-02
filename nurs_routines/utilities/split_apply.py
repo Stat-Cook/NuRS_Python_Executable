f563dfbcdd9be60ee90d2db2a7105b1b12980aa5
Rob <robert.cook@bcu.ac.uk>
2021-03-02 13:30:01 +0000

    Add temporary storage of index to retain original order.

--- nurs_routines/utilities/split_apply.py
+++ nurs_routines/utilities/split_apply.py
@@ -29,14 +29,18 @@ def split_apply(frm, groupby, func, sort_index=False):
     pandas.DataFrame
     """
     #grps = frm.groupby(groupby)
+    columns = list(frm.columns)
+    frm["__Raw_Index__"] = frm.index
     groups = frm.set_index(groupby).groupby(lambda x: x)
 
     merged = pd.concat(progress_bar_iter(groups, func))
+    merged = merged.reset_index().set_index("__Raw_Index__")
+    merged = merged.sort_index()
 
-    if sort_index:
-        return merged.sort_index()
-
-    return merged
+    columns += [i for i in merged.columns if i not in frm.columns]
+    # if sort_index:
+    #     return merged.sort_index()
+    return merged.reset_index(drop=True)[columns]
 
 
 def cached_split_apply(frm, groupby, func, file):
@@ -61,16 +65,24 @@ def cached_split_apply(frm, groupby, func, file):
     pandas.DataFrame
     """
     # groups = frm.groupby(groupby)
-    groups = frm.set_index(groupby).groupby(lambda x: x)
+    columns = list(frm.columns)
+    _frm = frm.copy()
+    _frm["__Raw_Index__"] = _frm.index
+    groups = _frm.set_index(groupby).groupby(lambda x: x)
 
     if os.path.isfile(file):
         os.remove(file)
 
     _iter = progress_bar_iter(groups, func)
     with open(file, "a") as output_file:
-        merge_in_file(output_file, _iter)
+        merge_in_file(output_file, _iter, index=True)
 
     result = pd.read_csv(file)
     os.remove(file)
 
-    return result
+    result = result.reset_index(drop=True).set_index("__Raw_Index__")
+    result = result.sort_index()
+
+    columns += [i for i in result.columns if i not in columns]
+
+    return result.reset_index(drop=True)[columns]
