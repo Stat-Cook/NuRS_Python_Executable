1382f146c138f911bcfe1740b41895a52e078fc8
Rob <robert.cook@bcu.ac.uk>
2021-03-02 16:50:52 +0000

    Read and display yaml at package load.

--- nurs_routines/config.py
+++ nurs_routines/config.py
@@ -1,4 +1,11 @@
 """
 Config file for global parameters.
 """
-EXTRACT_PATH = "Extract_data"
+import yaml
+
+# Config.yaml definitions:
+with open("config.yaml", "r") as f:
+    CONFIG_DICT = yaml.safe_load(f)
+
+EXTRACT_PATH = CONFIG_DICT.get("Extract Path")
+ALLOCATE_WARD_COLUMN = CONFIG_DICT.get("AllocateWardColumn")
