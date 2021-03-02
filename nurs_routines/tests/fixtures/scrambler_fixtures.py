2a222fa34253e9f85f137176c0281d8fd486d8e5
Rob <robert.cook@bcu.ac.uk>
2021-03-02 10:57:56 +0000

    Add tests for scrambling varying size data.

--- nurs_routines/tests/fixtures/scrambler_fixtures.py
+++ nurs_routines/tests/fixtures/scrambler_fixtures.py
@@ -8,8 +8,8 @@ import pandas as pd
 def scrambling_data():
     """Data frame fixture for testing scrambler."""
     return pd.DataFrame(dict(
-        ID=10*["A"] + 10*["B"],
-        ID2=2*(5*["A"] + 5*["B"]),
+        ID=10 * ["A"] + 10 * ["B"],
+        ID2=2 * (5 * ["A"] + 5 * ["B"]),
         value=range(20)
     ))
 
@@ -29,3 +29,14 @@ def scrambled_averages_id_id2():
         {("A", "A"): np.mean(range(5)), ("A", "B"): np.mean(range(5, 10)),
          ("B", "A"): np.mean(range(10, 15)), ("B", "B"): np.mean(range(15, 20))}
     )
+
+
+@pytest.fixture
+def mixed_size_scramble_data():
+    frame = pd.DataFrame(dict(
+        A=2 * ["A"] + list("CDEF") + 2 * ["B"] + list("GHIJKL") + 3 * ["A"] + 3 * ["B"],
+        B=4 * list(range(5)),
+        C=range(20)
+    ))
+    frame["A2"] = frame["A"]
+    return frame
--- nurs_routines/tests/test_scrambler.py
+++ nurs_routines/tests/test_scrambler.py
@@ -4,7 +4,7 @@ Unit tests for scramble functions
 import pandas as pd
 from nurs_routines.utilities.scrambler import scramble, scramble_to_file
 from nurs_routines.tests.fixtures.scrambler_fixtures import scrambling_data, \
-    scrambled_averages, scrambled_averages_id_id2
+    scrambled_averages, scrambled_averages_id_id2, mixed_size_scramble_data
 
 
 def test_scramble(scrambling_data, scrambled_averages):
@@ -46,3 +46,13 @@ def test_scramble_to_file_2_levels(scrambling_data, scrambled_averages_id_id2):
     groups = scrambled.groupby(["ID", "ID2"])
     averages = pd.Series({key: df["value"].mean() for key, df in groups})
     assert (averages == scrambled_averages_id_id2).values.all()
+
+
+def test_mixed_size_scramble_retains_index_order(mixed_size_scramble_data):
+    scrambled = scramble_to_file(mixed_size_scramble_data, ["A"], ["B", "C"], "temp")
+    assert all((scrambled["A"] == scrambled["A2"]).values)
+
+
+def test_mixed_size_scramble_check_data_removed(mixed_size_scramble_data):
+    scrambled = scramble_to_file(mixed_size_scramble_data, ["A"], ["B", "C"], "temp")
+    assert all(scrambled[scrambled["A"] == "C"]["B"].isna().values)
